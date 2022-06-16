c00790c3b77d7a20f3f01981270e6080ad4fe5b1
Yuriy Golota <ypgolota@pm.me>
2022-06-16 11:40:59 -0400

    removed stop words

--- word_frequency.py
+++ word_frequency.py
@@ -1,15 +1,43 @@
+from pickle import STOP
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+STOP_PUNCTUATION = [
+    '.', ',', '/', ';', '[', ']', '`', '-', '=', '<', '>', '?', ':', '"', '{', '}', 
+    '|', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')'
+]
+
 
 def print_word_freq(file):
     print(f'File name: {file}')
     with open(file) as open_file:
-        read_file = open_file.readlines()
-    print(read_file)
+        read_file = open_file.read()
+        # print(f'read_file:\n{read_file})
+
+    # Removing punctuation
+    file_without_punctuation = read_file.translate(str.maketrans('', '', string.punctuation))
+    # print(f'file_without_punctuation:\n{file_without_punctuation}')
+
+    # Lowercasing all characters
+    file_lowercased = file_without_punctuation.lower()
+    # print(f'file_lowercased:\n{file_lowercased}')
+
+    # Split long string into list of strings (where every index is a word)
+    file_split_into_list = str.split(file_lowercased)
+    # print(f'file_split_into_list:\n{file_split_into_list}')
+    # print(f'file_split_into_list is type:{type(file_split_into_list)}')
+
+    # Removing STOP_WORDS from the file
+    file_scrubbed = [word for word in file_split_into_list if word not in STOP_WORDS]
+    print(f'file_scrubbed:\n{file_scrubbed}')
+
+    
+
 
 
 if __name__ == "__main__":
@@ -27,3 +55,7 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+
+
+#read_file.string.split
+#translate
