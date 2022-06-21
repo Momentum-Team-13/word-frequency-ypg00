95068f63640aadfe747a916416bd01bf446057f4
Yuriy Golota <ypgolota@pm.me>
2022-06-21 10:31:57 -0400

    remove print statements

--- word_frequency.py
+++ word_frequency.py
@@ -5,33 +5,23 @@ STOP_WORDS = [
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
-STOP_PUNCTUATION = [
-    '.', ',', '/', ';', '[', ']', '`', '-', '=', '<', '>', '?', ':', '"', '{', '}', 
-    '|', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')'
-]
 
 def print_word_freq(file):
     print(f'File name: {file}')
     with open(file) as open_file:
         read_file = open_file.read()
-        # print(f'read_file:\n{read_file})
 
     # Removing punctuation
     file_without_punctuation = read_file.translate(str.maketrans('', '', string.punctuation))
-    # print(f'file_without_punctuation:\n{file_without_punctuation}')
 
     # Lowercasing all characters
     file_lowercased = file_without_punctuation.lower()
-    # print(f'file_lowercased:\n{file_lowercased}')
 
     # Split long string into list of strings (where every index is a word)
     file_split_into_list = str.split(file_lowercased)
-    # print(f'file_split_into_list:\n{file_split_into_list}')
-    # print(f'file_split_into_list is type:{type(file_split_into_list)}')
 
     # Removing STOP_WORDS from the file
     file_scrubbed = [word for word in file_split_into_list if word not in STOP_WORDS]
-    # print(f'file_scrubbed:\n{file_scrubbed}')
 
     # Fill dictionary with word counts
     word_count = {}
@@ -43,7 +33,6 @@ def print_word_freq(file):
 
     # Sorting
     sorted_list = sorted(word_count.items(), key=lambda x:x[1], reverse = True)
-    # print(sorted_list)  
 
     #Printing
     for word in sorted_list:
